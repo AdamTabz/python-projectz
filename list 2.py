662ace33185de1c4687ed8116c5972eb7f3022bd
AdamTabz <adam.tabannaj@icloud.com>
2026-06-16 18:51:23 +0200

    Add files via upload

---
+++ list 2.py
@@ -0,0 +1,9 @@
+list=[["new york","washington"],["france","italy"],["asia","afrika"]]
+print(list[1][1])
+for i in list:
+    print(i)
+    for j in i:
+        print(j)
+for i in range(3):
+   for j in range(2):
+           print(list[i][j])
