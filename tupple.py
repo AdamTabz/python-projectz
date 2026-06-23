42b87cb7e9dd1d8df6c599183e85e75fb9ba52e0
AdamTabz <adam.tabannaj@icloud.com>
2026-06-23 18:54:25 +0200

    Add files via upload

---
+++ tupple.py
@@ -0,0 +1,13 @@
+food=("burger", "fries", "eggs")
+print(food[2])
+#food[2]="poop"
+num={1,2,3,3,4}
+print(num)
+letter={"a","b","c","c"}
+print(letter)
+letter.add("e")
+print(letter)
+num2={3,4,5,6}
+print(num.intersection(num2))
+print(num.difference(num2))
+print(num.union(num2))
